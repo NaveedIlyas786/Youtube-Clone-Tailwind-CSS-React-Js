67ff26320c51fc9d390559023dbc065ab5847ad5
NaveedIlyas <naveedilyas321@gmail.com>
2023-06-27 15:06:53 +0500

    Update Feed.jsx

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -15,7 +15,7 @@ const Feed = () => {
       <div className="flex flex-row h-[calc(100%-56px)]">
           <LeftNav />
           <div className="grow w-[calc(100%-240px)] h-full overflow-y-auto bg-black">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-5">
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-5">
                   {!loading && searchResults && //! Means When ( loading && searchResults ) have become falsed then 'searchResults.map' should be loaded
                       searchResults.map((item,index) => {
                           if (item.type !== "video") return false;
